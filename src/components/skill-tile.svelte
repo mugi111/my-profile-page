1665ebb73f8c02cbfc3c0a286c7b68eb367a942b
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-07 01:13:47 +0900

    ADD: assetsフォルダの追加

--- src/components/skill-tile.svelte
+++ src/components/skill-tile.svelte
@@ -2,10 +2,11 @@
   export let imgSrc;
   export let textTitle;
   export let textBody;
+  let src = imgSrc;
 </script>
 
 <div class="tile-frame">
-  <img class="tile-img" alt="tile-img" src={imgSrc}>
+  <img class="tile-img" alt="tile-img" {src} />
   <div class="text-title-div">{textTitle}</div>
   <div class="text-body-div">{textBody}</div>
 </div>
--- src/components/skill.svelte
+++ src/components/skill.svelte
@@ -1,4 +1,10 @@
+<script>
+  import SkillTile from "./skill-tile";
+  import Kicad from "../assets/kicad-logo.png"
+</script>
+
 <h2 class="title-skill">Skill</h2>
+<SkillTile imgSrc={Kicad} textTitle="test" textBody="test desu" />
 
 <style>
   .title-skill {
