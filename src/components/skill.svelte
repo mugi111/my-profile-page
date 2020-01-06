9ea6e53ed5284c3c88e919752923367e31b098c8
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-07 01:54:52 +0900

    UPDATE: common lispの追加

--- src/components/skill.svelte
+++ src/components/skill.svelte
@@ -1,12 +1,14 @@
 <script>
   import SkillTile from "./skill-tile";
-  import Kicad from "../assets/kicad-logo.png"
-  import React from "../assets/react-logo.png"
+  import Kicad from "../assets/kicad-logo.png";
+  import React from "../assets/react-logo.png";
+  import CommonLisp from "../assets/common-lisp-logo.png";
 </script>
 
 <h2 class="title-skill">Skill</h2>
 <SkillTile imgSrc={Kicad} textTitle="KiCad" textBody="KiCadを使用した回路・基板設計" />
 <SkillTile imgSrc={React} textTitle="React" textBody="" />
+<SkillTile imgSrc={CommonLisp} textTitle="Common Lisp" textBody="" />
 
 <style>
   .title-skill {
