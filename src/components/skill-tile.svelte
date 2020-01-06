623491ffb6f9ec2e752404c1832106849a348388
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-07 01:38:01 +0900

    UPDATE: タイル表示用のstyle実装

--- src/components/skill-tile.svelte
+++ src/components/skill-tile.svelte
@@ -6,7 +6,29 @@
 </script>
 
 <div class="tile-frame">
-  <img class="tile-img" alt="tile-img" {src} />
+  <div class="img-div">
+    <img class="tile-img" alt="tile-img" {src} />
+  </div>
   <div class="text-title-div">{textTitle}</div>
   <div class="text-body-div">{textBody}</div>
 </div>
+
+<style>
+  .tile-frame {
+    width: 300px;
+    height: 300px;
+    border: 5px solid blue;
+    border-radius: 10px;
+  }
+
+  .img-div {
+    width: 300px;
+    height: 180px;
+    border-bottom: 3px dotted blue;
+  }
+  
+  .tile-img {
+    width: 100%;
+    height: auto;
+  }
+</style>
