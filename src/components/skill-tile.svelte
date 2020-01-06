9e1f4272679b1122eeff81ad320ad1d53477adf7
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-06 23:54:01 +0900

    UPDATE: classの追加

--- src/components/skill-tile.svelte
+++ src/components/skill-tile.svelte
@@ -4,7 +4,7 @@
   export let textBody;
 </script>
 
-<div>
+<div class="tile-frame">
   <img class="tile-img" alt="tile-img" src={imgSrc}>
   <div class="text-title-div">{textTitle}</div>
   <div class="text-body-div">{textBody}</div>
