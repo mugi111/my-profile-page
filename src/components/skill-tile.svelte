7cd06f65f0cb8d8222d5f36d9410f4b646c3c577
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-06 09:24:45 +0900

    ADD: タイルの作成

---
+++ src/components/skill-tile.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let imgSrc;
+  export let textTitle;
+  export let textBody;
+</script>
+
+<div>
+  <img class="tile-img" alt="tile-img" src={imgSrc}>
+  <div class="text-title-div">{textTitle}</div>
+  <div class="text-body-div">{textBody}</div>
+</div>
