789357a862feed5e6193e4c5b86d354fcc32224c
mugi <m.111_t.1230_nittc@outlook.com>
2020-01-07 01:53:52 +0900

    UPDATE: reactの追加

--- src/components/skill.svelte
+++ src/components/skill.svelte
@@ -1,10 +1,12 @@
 <script>
   import SkillTile from "./skill-tile";
   import Kicad from "../assets/kicad-logo.png"
+  import React from "../assets/react-logo.png"
 </script>
 
 <h2 class="title-skill">Skill</h2>
 <SkillTile imgSrc={Kicad} textTitle="KiCad" textBody="KiCadを使用した回路・基板設計" />
+<SkillTile imgSrc={React} textTitle="React" textBody="" />
 
 <style>
   .title-skill {
