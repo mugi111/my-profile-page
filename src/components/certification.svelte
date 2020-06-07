b5c46f393388a5de1d3af424e70a1c159af034af
mugi <m.111_t.1230_nittc@outlook.com>
2020-06-07 15:25:04 +0900

    update: certificateの追加と背景色の変更

--- src/components/certification.svelte
+++ src/components/certification.svelte
@@ -1,9 +1,11 @@
 <script>
-  import Az900 from "../assets/microsoft-certified-azure-fundamentals.png";
+  import Az900 from "../assets/Az900.png";
+  import Az203 from "../assets/Az203.png";
 </script>
 
 <h2 class="title-certification">Certificate</h2>
 <a rel="noreferrer" target="_blank" href="https://www.youracclaim.com/badges/8809fec5-27ba-47e4-8339-4ba2a8e59a19/public_url"><img class="az-900 official-logo" alt="az-900" src={Az900} /></a>
+<a rel="noreferrer" target="_blank" href="https://www.youracclaim.com/badges/83db44d3-61a2-4e60-a66b-4325b9bd2a16/public_url"><img class="az-203 official-logo" alt="az-203" src={Az203} /></a>
 
 <style>
   .title-certification {
@@ -16,6 +18,5 @@
     height: 120px;
     width: auto;
     margin: 20px;
-    background-color: white;
   }
 </style>
