4aff0db459cb80990951f53f605c1801fbb91f13
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2021-02-10 17:56:41 +0900

    add name

---
+++ src/components/ProfileAbout.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="profile-about">
+  <div class="profile-about-name">Hirunuma Takumi</div>
+  <div class="profile-about-job">Software Engineer</div>
+</div>
+
+<style>
+  .profile-about {
+    text-align: center;
+  }
+
+  .profile-about-job {
+    text-align: center;
+  }
+</style>
