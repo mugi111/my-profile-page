affe21b5d6919d5de25791064653c8383ff9582c
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2021-02-28 21:34:42 +0900

    remove

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -21,4 +21,16 @@
     left: 240px;
     top: 15px;
   }
+
+  @media (max-width: 640px) {
+    .rect {
+      position: absolute;
+      background-color: #ffc700;
+      width: 340px;
+      height: 270px;
+      right: 0px;
+      top: 180px;
+      display: none;
+    }
+  }
 </style>
